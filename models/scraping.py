642bbcc24c922d8805b194ae0faa27619567fba5
MohdAdnanKhan001 <adnankhanmak1786@gmail.com>
2025-04-23 01:15:12 +0530

    Modified the scraping script

--- models/scraping.py
+++ models/scraping.py
@@ -25,7 +25,7 @@ def fetch_top_comments(video_link, max_scrape=500, final_count=200):
     comments = []
     next_page_token = None
 
-    while len(comments) < max_scrape:
+    while True:
         request = youtube.commentThreads().list(
             part="snippet",
             videoId=video_id,
@@ -41,15 +41,16 @@ def fetch_top_comments(video_link, max_scrape=500, final_count=200):
                 'like_count': comment['likeCount'],
                 'text': comment['textDisplay']
             })
-            if len(comments) >= max_scrape:
-                break
 
         next_page_token = response.get('nextPageToken')
-        if not next_page_token:
+        
+        # Exit conditions
+        if not next_page_token or len(comments) >= max_scrape:
             break
 
+    # Adjust if scraped less than requested
     df = pd.DataFrame(comments)
-    df_sorted = df.sort_values(by='like_count', ascending=False).head(final_count)
+    df_sorted = df.sort_values(by='like_count', ascending=False).head(min(final_count, len(df)))
     df_sorted.reset_index(drop=True, inplace=True)
     df_sorted.index += 1
     df_sorted.rename(columns={'text': 'Comment'}, inplace=True)
